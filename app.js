48dd048e7c814326dd8bc307a99100f6ac805957
Soumya Dey <soumyadey200@hotmail.com>
2020-04-09 14:51:45 +0530

    added compose page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 
+// GLOBAL VARIABLES
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -10,17 +11,18 @@ const aboutContent =
     "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+let posts = [];
 
 // MIDDLEWARES
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
 
-
 // GET ROUTES
 app.get("/", (req, res) => {
     res.render("home", {
         homeContent: homeStartingContent,
+        postsArr: posts,
     });
 });
 
@@ -32,7 +34,7 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
     res.render("contact", {
-      contactContent: contactContent,
+        contactContent: contactContent,
     });
 });
 
@@ -40,6 +42,17 @@ app.get("/compose", (req, res) => {
     res.render("compose");
 });
 
+// POST ROUTES
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postContent,
+    };
+
+    posts.push(post);
+
+    res.redirect("/");
+});
 
 // LISTINING TO PORT
 app.listen(port, function () {
