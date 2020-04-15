33f9175a5d63902ef94ef1961bdf832ef97f7818
Soumya Dey <soumyadey200@hotmail.com>
2020-04-15 16:32:51 +0530

    added databse

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
-const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 // GLOBAL VARIABLES
 const app = express();
@@ -12,7 +12,21 @@ const aboutContent =
     "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+
+// DATABASE SETUP
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+// schema for storing a single item
+const postSchema = mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+const Post = mongoose.model("post", postSchema);
 
 // MIDDLEWARES
 app.set("view engine", "ejs");
@@ -21,9 +35,14 @@ app.use(express.static("static"));
 
 // GET ROUTES
 app.get("/", (req, res) => {
-    res.render("home", {
-        homeContent: homeStartingContent,
-        postsArr: posts,
+    Post.find({}, (err, results) => {
+        if (err) console.log(err);
+        else {
+            res.render("home", {
+                homeContent: homeStartingContent,
+                postsArr: results,
+            });
+        }
     });
 });
 
@@ -44,13 +63,11 @@ app.get("/compose", (req, res) => {
 });
 
 app.get("/posts/:postId", (req, res) => {
-    const reqPostId = lodash.lowerCase(req.params.postId);
-
-    posts.forEach((post) => {
-        if (lodash.lowerCase(post.title) === reqPostId) {
+    Post.findById(req.params.postId, (err, result) => {
+        if (err) console.log(err);
+        else {
             res.render("post", {
-                title: post.title,
-                content: post.content,
+                post: result,
             });
         }
     });
@@ -58,14 +75,22 @@ app.get("/posts/:postId", (req, res) => {
 
 // POST ROUTES
 app.post("/compose", (req, res) => {
-    const post = {
+    const newPost = Post({
         title: req.body.postTitle,
         content: req.body.postContent,
-    };
+    });
 
-    posts.push(post);
+    newPost.save((err) => {
+        if (err) console.log(err);
+        else res.redirect("/");
+    });
+});
 
-    res.redirect("/");
+app.post("/delete", (req, res) => {
+    Post.findByIdAndRemove(req.body.deleteBtn, (err) => {
+        if (err) console.log(err);
+        else res.redirect("/");
+    });
 });
 
 // LISTINING TO PORT
